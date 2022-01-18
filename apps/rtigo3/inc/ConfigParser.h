b351898c1c4843fa47eb798d9d9b214de5611424
Hassan MANSOURI <mansouri.hassan.ing@gmail.com>
2022-01-18 23:50:18 +0000

    add json parser

---
+++ apps/rtigo3/inc/ConfigParser.h
@@ -0,0 +1,42 @@
+
+#pragma once
+
+#ifndef CONFIG_PARSER_H
+#define CONFIG_PARSER_H
+
+#include <string>
+#if 0
+enum ParserTokenType
+{
+  PTT_UNKNOWN, // Unknown, normally indicates an error.
+  PTT_ID,      // Keywords, identifiers (not a number).
+  PTT_VAL,     // Immediate floating point value.
+  PTT_STRING,  // Filenames and any other identifiers in quotation marks.
+  PTT_EOL,     // End of line.
+  PTT_EOF      // End of file.
+};
+
+
+// System and scene file parsing information.
+class Parser
+{
+public:
+  Parser();
+  //~Parser();
+  
+  bool load(std::string const& filename);
+
+  ParserTokenType getNextToken(std::string& token);
+
+  size_t                 getSize() const;
+  std::string::size_type getIndex() const;
+  unsigned int           getLine() const;
+    
+private:
+  std::string            m_source; // System or scene description file contents.
+  std::string::size_type m_index;  // Parser's current character index into m_source.
+  unsigned int           m_line;   // Current source code line, one-based for error messages.
+  #endif
+};
+
+#endif // CONFIG_PARSER_H
---
+++ apps/rtigo3/src/ConfigParser.cpp
@@ -0,0 +1,153 @@
+
+#include "inc/ConfigParser.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+ConfigParser::ConfigParser()
+{
+}
+
+//ConfigParser::~ConfigParser()
+//{
+//}
+#if 0
+bool Parser::load(std::string const& filename)
+{
+  m_source.clear();
+
+  std::ifstream inputStream(filename);
+  if (!inputStream)
+  {
+    std::cerr << "ERROR: Parser::load() failed to open file " << filename << '\n';
+    return false;
+  }
+
+  std::stringstream data;
+
+  data << inputStream.rdbuf();
+
+  if (inputStream.fail())
+  {
+    std::cerr << "ERROR: loadString() Failed to read file " << filename << '\n';
+    return false;
+  }
+
+  m_source = data.str();
+  return true;
+}
+
+ParserTokenType Parser::getNextToken(std::string& token)
+{
+  const static std::string whitespace = " \t"; // space, tab
+  const static std::string value      = "+-0123456789.eE";
+  const static std::string delimiter  = " \t\r\n"; // space, tab, carriage return, linefeed
+  const static std::string newline    = "\n";
+  const static std::string quotation  = "\"";
+
+  token.clear(); // Make sure the returned token starts empty.
+
+  ParserTokenType type = PTT_UNKNOWN; // This return value indicates an error.
+
+  std::string::size_type first;
+  std::string::size_type last;
+
+  bool done = false;
+  while (!done)
+  {
+    // Find first character which is not a whitespace.
+    first = m_source.find_first_not_of(whitespace, m_index);
+    if (first == std::string::npos)
+    {
+      token = std::string();
+      type = PTT_EOF;
+      done = true;
+      continue;
+    }
+
+    // The found character indicates how parsing continues.
+    char c = m_source[first];
+
+    if (c == '#') // comment until the next newline
+    {
+      // m_index = first + 1; // skip '#' // Redundant.
+      first = m_source.find_first_of(newline, m_index); // Skip everything until the next newline.
+      if (first == std::string::npos)
+      {
+        type = PTT_EOF;
+        done = true;
+      }
+      m_index = first + 1; // skip newline
+      m_line++;
+    }
+    else if (c == '\r') // carriage return 13
+    {
+      m_index = first + 1;
+    }
+    else if (c == '\n') // newline (linefeed 10)
+    {
+      m_index = first + 1;
+      m_line++;
+    }
+    else if (c == '\"') // Quotation mark delimits strings (filenames or material names with spaces.)
+    {
+      ++first; // Skip beginning quotation mark.
+      last = m_source.find_first_of(quotation, first); // Find the ending quotation mark. Should be in the same line!
+      if (last == std::string::npos) // Error, no matching end quotation mark found.
+      { 
+        m_index = first; // Keep scanning behind the quotation mark.
+      }
+      else
+      {
+        m_index = last + 1; // Skip the ending quotation mark.
+        token = m_source.substr(first, last - first);
+        type = PTT_STRING;
+        done = true;
+      }
+    }
+    else // anything else
+    {
+      last = m_source.find_first_of(delimiter, first);
+      if (last == std::string::npos) 
+      { 
+        last = m_source.size();
+      }
+      m_index = last;
+      token = m_source.substr(first, last - first);
+      type = PTT_ID; // Default to general identifier.
+      // Check if token is only built of characters used for numbers. 
+      // (Not perfectly parsing a floating point number but good enough for most filenames.)
+      if (isdigit(c) || c == '-' || c == '+' || c == '.') // Legal start characters for a floating point number.
+      {
+        last = token.find_first_not_of(value, 0);
+        if (last == std::string::npos) 
+        { 
+          type = PTT_VAL;
+        }
+      }
+      done = true;
+    }
+  }
+
+  return type;
+}
+
+std::string::size_type Parser::getSize() const
+{
+  return m_source.size();
+}
+
+std::string::size_type Parser::getIndex() const
+{
+  return m_index;
+}
+
+unsigned int Parser::getLine() const
+{
+  return m_line;
+}
+
+#endif
+
